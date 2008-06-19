982b082751e59d0287b389ce0bfbeeff3d0291ad
staninprague <staninprague@4c390980-e74c-0410-85aa-cbd269a6c1b3>
2008-06-19 13:57:39 +0000

    Creating projects structure for the Tools.Collaboration

---
+++ trunk/Tools.Net/src/Tools.Collaboration.Publishing/Publisher.cs
@@ -0,0 +1,32 @@
+﻿using Tools.Collaboration.Contracts;
+
+namespace Tools.Collaboration.Publishing
+{
+    public class Publisher : IPublisher
+    {
+
+        #region IPublisher Members
+
+        public string GetName()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Publish(string message)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Publish(string message, string activityId)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void AddSubscriber(ISubscriber subscriber)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        #endregion
+    }
+}
