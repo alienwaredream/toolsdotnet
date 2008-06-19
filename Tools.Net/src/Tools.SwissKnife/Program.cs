ee4c06879d5ed19f53e72a8d1134914f271d287b
staninprague <staninprague@4c390980-e74c-0410-85aa-cbd269a6c1b3>
2008-06-19 12:16:33 +0000

    Adding utilities console as per [http://code.google.com/p/toolsdotnet/wiki/ToolsSwissKnife] page

---
+++ Tools.Net/src/Tools.SwissKnife/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tools.SwissKnife
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                if ((! (args.Length > 0)) || args[0] == "/?") 
+                {
+                    System.Console.WriteLine("Usage: tools.swissknife command params");
+                    System.Console.WriteLine("Commands:");
+                    System.Console.WriteLine("SetFileModifiedDate|sfmd");
+                    System.Console.WriteLine("ProtectConfigSection|pcs");
+                    System.Console.WriteLine("GetUserSid|gus");
+                    System.Console.WriteLine("Use no parameters to get help on a command.");
+                    //TODO: (SD) Add usage info
+                    return;
+                }
+                string[] argsProxy = new string[args.Length - 1];
+                for (int i = 1; i < args.Length; i++) { argsProxy[i - 1] = args[i]; }
+
+                switch (args[0])
+                {
+                    case "sfmd" :
+                    case "SetFileModifiedDate": SetFileModifiedDate.Main(argsProxy);
+                        break;
+                    case "pcs" :
+                    case "ProtectConfigSection": ProtectConfigSection.Main(argsProxy);
+                        break;
+                    case "gus" :
+                    case "GetUserSid": GetUserSid.Main(argsProxy);
+                        break;
+                    default: throw new Exception("Incorrect usage!");
+                }
+                //System.Console.WriteLine("Press any key to exit.");
+                //System.Console.ReadKey();
+            }
+            catch (Exception ex)
+            {
+                System.Console.ForegroundColor = ConsoleColor.Red;
+                System.Console.WriteLine(ex.ToString());
+                System.Console.ResetColor();
+            }
+        }
+    }
+}
---
+++ Tools.Net/src/Tools.SwissKnife/ProtectConfigSection.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Configuration;
+using System.Globalization;
+
+namespace Tools.SwissKnife
+{
+    class ProtectConfigSection
+    {
+        internal static void Main(string[] args)
+        {
+            if (args.Length == 0 || args.Length < 3)
+            {
+                
+                System.Console.WriteLine(" -p|-u|-s \"Path.exe.config|Path.web.config\" \"sectionName\"");
+                System.Console.WriteLine("To protect: -p \"c:\\myapp\\myapp.exe.config\" \"mySection\"");
+                System.Console.WriteLine("To unprotect: -u \"c:\\myapp\\myapp.exe.config\" \"mySection\"");
+                System.Console.WriteLine("To show: -s \"c:\\myapp\\myapp.exe.config\" \"mySection\"");
+                return;
+            }
+
+            string command = args[0];
+            string configFilePath = args[1];
+            string sectionName = args[2];
+
+            // Get the current configuration file.
+            ExeConfigurationFileMap map = new ExeConfigurationFileMap();
+            map.ExeConfigFilename = configFilePath;
+            
+            System.Configuration.Configuration config =
+                    ConfigurationManager.OpenMappedExeConfiguration(map, ConfigurationUserLevel.None);
+
+
+            // Get the section.
+            ConfigurationSection section =
+                config.GetSection(sectionName.Trim('\"', ' '));
+
+            if (section == null)
+                throw new Exception(String.Format(CultureInfo.InvariantCulture,
+                    "Section {0} can't be found in the file {1}", sectionName, configFilePath));
+
+            if (command == "-p")
+            {
+                // Protect (encrypt)the section.
+                section.SectionInformation.ProtectSection(
+                    "RsaProtectedConfigurationProvider");
+                SaveSection(config, section);
+                return;
+            }
+            if (command == "-u")
+            {
+                // Protect (encrypt)the section.
+                section.SectionInformation.UnprotectSection();
+                SaveSection(config, section);
+                return;
+            }
+            if (command == "-s")
+            {
+
+                // Display decrypted configuration 
+                // section. Note, the system
+                // uses the Rsa provider to decrypt
+                // the section transparently.
+                string sectionXml =
+                    section.SectionInformation.GetRawXml();
+
+                System.Console.WriteLine("Decrypted section:");
+                System.Console.WriteLine(sectionXml);
+                return;
+            }
+            throw new Exception("Incorrect usage!");
+        }
+
+        private static void SaveSection(System.Configuration.Configuration config, ConfigurationSection section)
+        {
+            // Save the section.
+            section.SectionInformation.ForceSave = true;
+
+            config.Save(ConfigurationSaveMode.Full);
+        }
+    }
+}
