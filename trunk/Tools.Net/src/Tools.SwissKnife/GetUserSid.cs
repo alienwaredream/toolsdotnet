2e95a531ae43ade0a402dfd99f809e7fa768a5dd
staninprague <staninprague@4c390980-e74c-0410-85aa-cbd269a6c1b3>
2008-06-19 12:16:33 +0000

    Adding utilities console as per [http://code.google.com/p/toolsdotnet/wiki/ToolsSwissKnife] page

---
+++ trunk/Tools.Net/src/Tools.SwissKnife/GetUserSid.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using System.Globalization;
+using System.Security.Principal;
+
+namespace Tools.SwissKnife
+{
+    class GetUserSid
+    {
+        internal static void Main(string[] args)
+        {
+            if (args.Length < 1)
+            {
+                System.Console.WriteLine(
+                    "Usage: \"domain\\accountName\"");
+                System.Console.WriteLine(
+                    "Sample: \"localPC\\localAccount\"");
+                System.Console.WriteLine(
+                    "Sample: \"domain\\domainAccount\"");
+                return;
+
+            }
+
+            NTAccount account = new NTAccount(args[0]);
+
+            System.Console.WriteLine("SID: " + account.Translate(typeof(System.Security.Principal.SecurityIdentifier)).Value);
+        }
+    }
+}
---
+++ trunk/Tools.Net/src/Tools.SwissKnife/SetFileModifiedDate.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using System.Globalization;
+
+namespace Tools.SwissKnife
+{
+    class SetFileModifiedDate
+    {
+        internal static void Main(string[] args)
+        {
+            if (args.Length < 1)
+            {
+                System.Console.WriteLine(String.Format(CultureInfo.InvariantCulture,
+                    "Usage: SetFileModifiedDate \"FilePath\""));
+                return;
+
+            }
+            string filePath = args[0];
+
+            if (!File.Exists(filePath))
+            {
+                System.Console.WriteLine("File: \"" + filePath + "\" doesn't exist!");
+                return;
+
+            }
+            FileAttributes attr = File.GetAttributes(filePath);
+
+            if ((attr & FileAttributes.ReadOnly) > 0)
+            {
+                System.Console.WriteLine("File: \"" + filePath + "\" is readonly!");
+                return;
+            }
+            FileInfo fi = new FileInfo(filePath);
+            fi.LastWriteTime = DateTime.Now.AddDays(-1);
+
+            System.Console.WriteLine("Modified date is now set to: " + fi.LastWriteTime.ToString("ddMMMyyTHH:mm:ss fff"));
+        }
+    }
+}
