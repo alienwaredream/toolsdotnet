bbf9d68c21be4ec0940c81e7c6c87df240c1fa7b
staninprague <staninprague@4c390980-e74c-0410-85aa-cbd269a6c1b3>
2008-07-10 07:51:34 +0000

    Applying datacontract attributes.

--- Tools.Net/src/Tools.Collaboration.Contracts/SubscriberData.cs
+++ Tools.Net/src/Tools.Collaboration.Contracts/SubscriberData.cs
@@ -2,12 +2,16 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Runtime.Serialization;
 
 namespace Tools.Collaboration.Contracts
 {
+    [DataContract()]
     public class SubscriberData
     {
+        [DataMember()]
         public string Url { get; set; }
+        [DataMember()]
         public string Name { get; set; }
     }
 }
