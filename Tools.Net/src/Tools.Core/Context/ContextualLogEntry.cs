332c47ed051f518069ffcf3d3855f986ad3a27c8
staninprague <staninprague@4c390980-e74c-0410-85aa-cbd269a6c1b3>
2008-07-24 08:49:50 +0000

    Preparing for the queueservices integration into the project.

---
+++ Tools.Net/src/Tools.Core/Context/ContextualLogEntry.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.Serialization;
+
+namespace Tools.Core.Context
+{
+    [Serializable()]
+    [DataContract()]
+    public class ContextualLogEntry : IContextIdentifierHolder
+    {
+        [DataMember()]
+        public string Message { get; set; }
+
+        #region IContextIdentifierHolder Members
+
+        [DataMember()]
+        public ContextIdentifier ContextIdentifier
+        {
+            get;
+            set;
+        }
+
+        #endregion
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.Append("Message:").Append(Message);
+
+            return sb.ToString();
+        }
+    }
+}
---
+++ Tools.Net/src/Tools.Core/VoidDelegate.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace Tools.Core
+{
+	/// <summary>
+	/// Represents a delegate with the void return type and void parameters list.
+    /// Matter of compat with 2.0.
+	/// </summary>
+	public delegate void VoidDelegate();
+}
