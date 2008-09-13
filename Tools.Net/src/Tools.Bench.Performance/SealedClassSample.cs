59541efd57646ce61932a87768da782ee8bd21e4
staninprague <staninprague@4c390980-e74c-0410-85aa-cbd269a6c1b3>
2008-09-13 15:54:58 +0000

    new project to test call and callvirt usage

---
+++ Tools.Net/src/Tools.Bench.Performance/SealedClassSample.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Tools.Bench.Performance
+{
+    sealed class SealedClassSample
+    {
+        internal void DoWork() { int i = 0; i = i + 1; }
+    }
+}
