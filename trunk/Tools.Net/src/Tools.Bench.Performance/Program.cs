4d63888d9c8c5cdfd842ffe4441def4a39f973a1
staninprague <staninprague@4c390980-e74c-0410-85aa-cbd269a6c1b3>
2008-09-13 15:54:58 +0000

    new project to test call and callvirt usage

---
+++ trunk/Tools.Net/src/Tools.Bench.Performance/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Tools.Bench.Performance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Press any key to start");
+            Console.Read();
+
+            new SealedExampleRunner().Run();
+            new SealedExampleRunner().Run();
+            new SealedExampleRunner().Run();
+            new SealedExampleRunner().Run();
+
+            Console.WriteLine("Press any key to exit");
+            Console.Read();
+            Console.Read();
+        }
+    }
+}
---
+++ trunk/Tools.Net/src/Tools.Bench.Performance/SealedExampleRunner.cs
@@ -0,0 +1,43 @@
+﻿using System.Diagnostics;
+using System;
+
+namespace Tools.Bench.Performance
+{
+    class SealedExampleRunner
+    {
+        private const int NumberOfIterations = 100000000;
+
+        internal void Run()
+        {
+            Stopwatch stopwatch = new Stopwatch();
+            
+            NonSealedClassSample nonSealedInstance = new NonSealedClassSample();
+            SealedClassSample sealedInstance = new SealedClassSample();
+
+            stopwatch.Start();
+
+            for (int i = 0; i < NumberOfIterations; i++)
+            {
+                sealedInstance.DoWork();
+            }
+            stopwatch.Stop();
+
+            Console.WriteLine(String.Format("Executed sealed for {0} iterations: {1}ms", NumberOfIterations,
+                stopwatch.ElapsedMilliseconds));
+
+            
+
+            stopwatch.Reset();
+            stopwatch.Start();
+
+            for (int i = 0; i < NumberOfIterations; i++)
+            {
+                nonSealedInstance.DoWork();
+            }
+            stopwatch.Stop();
+
+            Console.WriteLine(String.Format("Executed non sealed for {0} iterations: {1}ms", NumberOfIterations,
+                stopwatch.ElapsedMilliseconds));
+        }
+    }
+}
