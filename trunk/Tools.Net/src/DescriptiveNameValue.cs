5942d9d52c8fa32ad8ccf7723d972014dff3e719
staninprague <staninprague@4c390980-e74c-0410-85aa-cbd269a6c1b3>
2008-10-19 09:53:13 +0000

    fixing havoc of the svn

---
+++ trunk/Tools.Net/src/DescriptiveNameValue.cs
@@ -0,0 +1,69 @@
+using System;
+using System.Xml.Serialization;
+
+namespace Tools.Core
+{
+	/// <summary>
+	/// Summary description for DescriptiveNameValue.
+	/// </summary>
+	[Serializable]
+	public class DescriptiveNameValue : Descriptor, ICloneable
+	{
+		
+		#region Fields
+		
+		private string _value;
+
+		#endregion
+
+		#region Properties
+		/// <summary>
+		/// Value.
+		/// </summary>
+        [XmlAttribute]
+        public virtual string Value
+        {
+            get;
+            set;
+        }
+
+		#endregion
+
+		#region Constructors
+		
+		public DescriptiveNameValue() : base() {}
+
+		public DescriptiveNameValue(string name, string val, string description) 
+			: base(name, description)
+		{
+			_value = val;
+		}
+
+		#endregion
+		
+		#region Methods
+
+		public override string ToString()
+		{
+			return Value;
+		}
+
+
+		#endregion
+
+
+		#region ICloneable Members
+
+		public object Clone()
+		{
+			return new DescriptiveNameValue
+			(
+			Name,
+			Value,
+			Description
+			);
+		}
+
+		#endregion
+}
+}
