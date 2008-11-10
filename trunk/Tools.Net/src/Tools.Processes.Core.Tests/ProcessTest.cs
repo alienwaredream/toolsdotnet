c0db9d109bfd177c1313bc8dc5264d8d6d259f7e
staninprague <staninprague@4c390980-e74c-0410-85aa-cbd269a6c1b3>
2008-11-10 06:15:06 +0000

    Adding Tools.Processes.Core tests. Updating reference for Rhino.Mocks

---
+++ trunk/Tools.Net/src/Tools.Processes.Core.Tests/ProcessTest.cs
@@ -0,0 +1,352 @@
+﻿using Rhino.Mocks;
+using Tools.Processes.Core;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Threading;
+
+namespace Tools.Processes.Core.Tests
+{
+
+
+    /// <summary>
+    ///This is a test class for ProcessTest and is intended
+    ///to contain all ProcessTest Unit Tests
+    ///</summary>
+    [TestClass]
+    public class ProcessTest
+    {
+
+
+        private TestContext testContextInstance;
+
+        /// <summary>
+        ///Gets or sets the test context which provides
+        ///information about and functionality for the current test run.
+        ///</summary>
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+
+        #region Additional test attributes
+        // 
+        //You can use the following additional attributes as you write your tests:
+        //
+        //Use ClassInitialize to run code before running the first test in the class
+        //[ClassInitialize()]
+        //public static void MyClassInitialize(TestContext testContext)
+        //{
+        //}
+        //
+        //Use ClassCleanup to run code after all tests in a class have run
+        //[ClassCleanup()]
+        //public static void MyClassCleanup()
+        //{
+        //}
+        //
+        //Use TestInitialize to run code before running each test
+        //[TestInitialize()]
+        //public void MyTestInitialize()
+        //{
+        //}
+        //
+        //Use TestCleanup to run code after each test has run
+        //[TestCleanup()]
+        //public void MyTestCleanup()
+        //{
+        //}
+        //
+        #endregion
+
+
+        /// <summary>
+        ///A test for Name
+        ///</summary>
+        [TestMethod]
+        public void NameTest()
+        {
+            Process target = CreateProcess();
+            string expected = "TestOfName";
+            string actual;
+            target.Name = expected;
+            actual = target.Name;
+            Assert.AreEqual(expected, actual);
+        }
+
+        /// <summary>
+        ///A test for ExecutionStateSyncObj
+        ///</summary>
+        [TestMethod]
+        public void ExecutionStateSyncObjTest()
+        {
+            Assert.IsNotNull(CreateProcess().ExecutionStateSyncObj);
+        }
+
+        /// <summary>
+        ///A test for ExecutionState
+        ///</summary>
+        [TestMethod]
+        public void ExecutionStateTest()
+        {
+            Assert.AreEqual<ProcessExecutionState>(CreateProcess().ExecutionState, ProcessExecutionState.Unstarted);
+        }
+
+        /// <summary>
+        ///A test for Description
+        ///</summary>
+        [TestMethod]
+        public void DescriptionTest()
+        {
+            Assert.AreEqual<string>("TestOfDescription", new MockProcess { Description = "TestOfDescription" }.Description);
+        }
+
+        /// <summary>
+        ///A test for CompletedHandle
+        ///</summary>
+        [TestMethod]
+        public void CompletedHandleTest()
+        {
+            Assert.IsNotNull(CreateProcess().CompletedHandle);
+        }
+
+        /// <summary>
+        ///A test for CompletedEvent
+        ///</summary>
+        [TestMethod]
+        [DeploymentItem("Tools.Processes.Core.dll")]
+        public void CompletedEventTest()
+        {
+            var param0 = new PrivateObject(new MockProcess());
+            var target = new Process_Accessor(param0);
+            Assert.IsNotNull(target.CompletedEvent);
+        }
+
+        /// <summary>
+        ///A test for Suspend
+        ///</summary>
+        [TestMethod]
+        [ExpectedException(typeof(NotImplementedException))]
+        public void SuspendTest()
+        {
+            Process target = CreateProcess(); // TODO: Initialize to an appropriate value
+            target.Suspend();
+        }
+
+        /// <summary>
+        ///A test for Stop
+        ///</summary>
+        [TestMethod]
+        public void StopTest()
+        {
+            var param0 = new PrivateObject(new MockProcess());
+            var process = new Process_Accessor(param0);
+
+            //var resetEvent = MockRepository.GenerateStub<EventWaitHandle>(false, EventResetMode.ManualReset);
+
+            //process.completedEvent = resetEvent;
+            //resetEvent.Stub(r => r.Set());
+
+
+
+            bool stoppingEventCalled = false;
+            bool stoppedEventCalled = false;
+
+            process.add_Stopping((s, a) =>
+                                    {
+                                        stoppingEventCalled = true;
+                                        Assert.AreEqual<ProcessExecutionState>(ProcessExecutionState.StopRequested, process.ExecutionState);
+                                    });
+            process.add_Stopped((s, a) =>
+                                    {
+                                        stoppedEventCalled = true;
+                                        Assert.AreEqual<ProcessExecutionState>(ProcessExecutionState.Stopped,
+                                                                               process.ExecutionState);
+                                    });
+
+            process.Stop();
+
+            //resetEvent.AssertWasCalled(r => r.Set());
+
+            Assert.IsTrue(stoppingEventCalled);
+            Assert.IsTrue(stoppedEventCalled);
+
+            Assert.AreEqual<ProcessExecutionState>(ProcessExecutionState.Stopped, process.ExecutionState);
+
+        }
+
+        /// <summary>
+        ///A test for Start
+        ///</summary>
+        [TestMethod]
+        public void StartTest()
+        {
+            Process target = CreateProcess();
+            target.Start();
+        }
+
+        /// <summary>
+        ///A test for SetExecutionState
+        ///</summary>
+        [TestMethod]
+        [DeploymentItem("Tools.Processes.Core.dll")]
+        public void SetExecutionStateTest()
+        {
+            var param0 = new PrivateObject(CreateProcess());
+            var target = new Process_Accessor(param0);
+            ProcessExecutionState state = ProcessExecutionState.Finished;
+            target.SetExecutionState(state);
+            Assert.AreEqual<ProcessExecutionState>(target.ExecutionState, state);
+            // changing in case the initial value was equal to the previous state.
+            state = ProcessExecutionState.Stopped;
+            target.SetExecutionState(state);
+            Assert.AreEqual<ProcessExecutionState>(target.ExecutionState, state);
+        }
+
+        /// <summary>
+        ///A test for Resume
+        ///</summary>
+        [TestMethod]
+        [ExpectedException(typeof(NotImplementedException))]
+        public void ResumeTest()
+        {
+            Process target = CreateProcess(); // TODO: Initialize to an appropriate value
+            target.Resume();
+        }
+
+        /// <summary>
+        ///A test for OnTerminated
+        ///</summary>
+        [TestMethod]
+        [DeploymentItem("Tools.Processes.Core.dll")]
+        public void OnTerminatedTest()
+        {
+            PrivateObject param0 = new PrivateObject(CreateProcess());
+            Process_Accessor target = new Process_Accessor(param0);
+            ProcessExitEventArgs eventArgs = new ProcessExitEventArgs {CompletionState = new {Field = 123}};
+
+            bool terminatedEventCalled = false;
+
+            target.add_Terminated((s, e)=>
+                                      {
+                                          terminatedEventCalled = true;
+                                          Assert.AreEqual<ProcessExecutionState>(ProcessExecutionState.Terminated, target.ExecutionState);
+                                      });
+
+            target.OnTerminated(eventArgs);
+
+            Assert.IsTrue(terminatedEventCalled);
+        }
+
+        internal virtual Process_Accessor CreateProcess_Accessor()
+        {
+            return new Process_Accessor(new PrivateObject(CreateProcess()));
+        }
+
+        /// <summary>
+        ///A test for OnCompleted
+        ///</summary>
+        [TestMethod]
+        [DeploymentItem("Tools.Processes.Core.dll")]
+        public void OnCompletedTest()
+        {
+
+            var target = CreateProcess_Accessor();
+            var eventArgs = new ProcessExitEventArgs {CompletionState = new {Field = 123}};
+
+            bool completedEventCalled = false;
+
+            target.add_Completed((s, e) =>
+            {
+                completedEventCalled = true;
+                Assert.AreEqual<ProcessExecutionState>(ProcessExecutionState.Completed, target.ExecutionState);
+            });
+
+            target.OnCompleted(eventArgs);
+
+            Assert.IsTrue(completedEventCalled);
+        }
+
+        /// <summary>
+        ///A test for Initialize
+        ///</summary>
+        [TestMethod]
+        public void InitializeTest()
+        {
+            Process target = CreateProcess();
+            target.Initialize();
+        }
+
+        /// <summary>
+        ///A test for EndStop
+        ///</summary>
+        [TestMethod]
+        [ExpectedException(typeof(NotImplementedException))]
+        public void EndStopTest()
+        {
+            Process target = CreateProcess(); // TODO: Initialize to an appropriate value
+            IAsyncResult ar = null; // TODO: Initialize to an appropriate value
+            target.EndStop(ar);
+        }
+
+        /// <summary>
+        ///A test for Dispose
+        ///</summary>
+        [TestMethod]
+        public void DisposeTest()
+        {
+            Process target = CreateProcess(); // TODO: Initialize to an appropriate value
+            target.Dispose();
+            target.Dispose();
+        }
+
+        /// <summary>
+        ///A test for BeginStop
+        ///</summary>
+        [TestMethod]
+        [ExpectedException(typeof(NotImplementedException))]
+        public void BeginStopTest()
+        {
+            Process target = CreateProcess(); // TODO: Initialize to an appropriate value
+            object state = null; // TODO: Initialize to an appropriate value
+            AsyncCallback callback = null; // TODO: Initialize to an appropriate value
+            IAsyncResult expected = null; // TODO: Initialize to an appropriate value
+            IAsyncResult actual;
+            actual = target.BeginStop(state, callback);
+            Assert.AreEqual(expected, actual);
+            Assert.Inconclusive("Verify the correctness of this test method.");
+        }
+
+        internal virtual Process CreateProcess()
+        {
+            Process target = new MockProcess();
+            return target;
+        }
+
+        /// <summary>
+        ///A test for Abort
+        ///</summary>
+        [TestMethod]
+
+        public void AbortTest()
+        {
+            Process target = CreateProcess(); // TODO: Initialize to an appropriate value
+            target.Abort();
+            Assert.AreEqual<ProcessExecutionState>(ProcessExecutionState.AbortRequested, target.ExecutionState);
+        }
+    }
+
+    class MockProcess : Process
+    {
+        public override void Start()
+        {
+        }
+    }
+
+}
---
+++ trunk/Tools.Net/src/Tools.Processes.Core.Tests/ThreadedProcessTest.cs
@@ -0,0 +1,272 @@
+﻿using System.Diagnostics;
+using System.Threading;
+using Tools.Processes.Core;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using ThreadState=System.Threading.ThreadState;
+using System.Runtime.CompilerServices;
+
+namespace Tools.Processes.Core.Tests
+{
+    
+    
+    /// <summary>
+    ///This is a test class for ThreadedProcessTest and is intended
+    ///to contain all ThreadedProcessTest Unit Tests
+    ///</summary>
+    [TestClass]
+    public class ThreadedProcessTest
+    {
+
+        private bool beginStopCallbackCalled;
+        private TestContext testContextInstance;
+        private EventWaitHandle beginStopCallbackHandle = new EventWaitHandle(false, EventResetMode.AutoReset);
+
+        /// <summary>
+        ///Gets or sets the test context which provides
+        ///information about and functionality for the current test run.
+        ///</summary>
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+
+        #region Additional test attributes
+        // 
+        //You can use the following additional attributes as you write your tests:
+        //
+        //Use ClassInitialize to run code before running the first test in the class
+        //[ClassInitialize()]
+        //public static void MyClassInitialize(TestContext testContext)
+        //{
+        //}
+        //
+        //Use ClassCleanup to run code after all tests in a class have run
+        //[ClassCleanup()]
+        //public static void MyClassCleanup()
+        //{
+        //}
+        //
+        //Use TestInitialize to run code before running each test
+        //[TestInitialize()]
+        //public void MyTestInitialize()
+        //{
+        //}
+        //
+        //Use TestCleanup to run code after each test has run
+        //[TestCleanup()]
+        //public void MyTestCleanup()
+        //{
+        //}
+        //
+        #endregion
+
+
+        /// <summary>
+        ///A test for WorkingThread
+        ///</summary>
+        [TestMethod]
+        [DeploymentItem("Tools.Processes.Core.dll")]
+        public void WorkingThreadTest()
+        {
+            ThreadedProcess_Accessor target = CreateThreadedProcess_Accessor();
+            target.Start();
+            Assert.IsNotNull(target.WorkingThread);
+        }
+
+        /// <summary>
+        ///A test for OperationReset
+        ///</summary>
+        [TestMethod]
+        public void OperationResetTest()
+        {
+            Assert.IsNotNull(CreateThreadedProcess_Accessor().OperationReset);
+        }
+
+        /// <summary>
+        ///A test for Stop
+        ///</summary>
+        [TestMethod]
+        public void EmptyStopTest()
+        {
+            ThreadedProcess_Accessor target = CreateThreadedProcess_Accessor();
+
+            bool terminatedEventCalled = false;
+
+            target.Start();
+
+            target.add_Terminated((s, e) =>
+                                      {
+                                          terminatedEventCalled = true;
+                                          Assert.AreEqual<ProcessExecutionState>(ProcessExecutionState.Terminated, target.ExecutionState);
+                                      });
+            target.Stop();
+
+            Assert.IsTrue(terminatedEventCalled);
+            Assert.IsTrue(target.WorkingThread.ThreadState == ThreadState.Stopped);
+            Assert.AreEqual<ProcessExecutionState>(ProcessExecutionState.Terminated, target.ExecutionState);
+        }
+
+        /// <summary>
+        ///A test for Start
+        ///</summary>
+        [TestMethod]
+        public void StartTest()
+        {
+            var process = new MockThreadedBeginStopWithSleepingThreadProcess();
+            var target = new ThreadedProcess_Accessor(new PrivateObject(process));
+
+            target.Start();
+
+            Assert.AreEqual(ProcessExecutionState.Running, process.ExecutionState);
+
+            target.Stop();
+
+            Assert.AreEqual(ProcessExecutionState.Terminated, process.ExecutionState);
+            Assert.IsTrue(target.WorkingThread.ThreadState == ThreadState.Stopped);
+        }
+
+        /// <summary>
+        ///A test for SelfSuspend
+        ///</summary>
+        [TestMethod]
+        [DeploymentItem("Tools.Processes.Core.dll")]
+        public void SelfSuspendTest()
+        {
+            // time to suspend, ms.
+            const int suspendTimeout = 5000;
+            MockThreadedSuspendProcess process = new MockThreadedSuspendProcess(suspendTimeout);
+            ThreadedProcess_Accessor target = new ThreadedProcess_Accessor(new PrivateObject(process ));
+
+            target.Start();
+            // give it some time before calling stop, or if it completes it will set its event (not now :))
+            target.CompletedEvent.WaitOne((int)(suspendTimeout*2));
+            
+            target.Stop();
+
+            Assert.IsTrue(process.StopWatch.ElapsedMilliseconds >= suspendTimeout);
+            Assert.IsTrue(!process.ResumedBeforeTimeout);
+        }
+
+        internal virtual ThreadedProcess_Accessor CreateThreadedProcess_Accessor()
+        {
+            return new ThreadedProcess_Accessor(new PrivateObject(CreateThreadedProcess()));
+        }
+
+        /// <summary>
+        ///A test for BeginStop
+        ///</summary>
+        [TestMethod]
+        public void BeginStopTest()
+        {
+            var process = new MockThreadedBeginStopWithSleepingThreadProcess();
+            var target = new ThreadedProcess_Accessor(new PrivateObject(process));
+
+            var state = new MockAsyncState {TestField = "TestFieldValue"};
+
+            target.Start();
+
+            IAsyncResult ar = target.BeginStop(state, StopCallback);
+
+            if (!ar.AsyncWaitHandle.WaitOne(20000))
+            {
+                Assert.IsTrue(false, "The time given to the process to stop has been exceeded!");
+            }
+
+            Assert.IsTrue(ar.IsCompleted);
+            Assert.IsTrue(process.ThreadWasInterrupted, "Thread should have been interrupted from its WaitSleepJoin state!");
+            beginStopCallbackHandle.WaitOne();
+            Assert.IsTrue(beginStopCallbackCalled, "The BeginStopCallback should have been called!");
+        }
+
+        private void StopCallback(IAsyncResult ar)
+        {
+            beginStopCallbackCalled = true;
+            Assert.IsNotNull(ar.AsyncState);
+            Assert.IsNotNull(ar.AsyncState as MockAsyncState);
+            Assert.AreEqual((ar.AsyncState as MockAsyncState).TestField, "TestFieldValue");
+            beginStopCallbackHandle.Set();
+        }
+
+        internal virtual ThreadedProcess CreateThreadedProcess()
+        {
+            return new MockThreadedProcess();
+        }
+
+        /// <summary>
+        ///A test for Abort
+        ///</summary>
+        [TestMethod]
+        public void AbortTest()
+        {
+            var process = new MockThreadedBeginStopWithSleepingThreadProcess();
+            var target = new ThreadedProcess_Accessor(new PrivateObject(process));
+
+            target.Start();
+
+            Assert.AreEqual(ProcessExecutionState.Running, process.ExecutionState);
+
+            target.Abort();
+
+            Assert.AreEqual(ProcessExecutionState.Terminated, process.ExecutionState);
+            Assert.AreEqual(ThreadState.Stopped, target.WorkingThread.ThreadState);
+        }
+    }
+    class MockThreadedProcess : ThreadedProcess
+    {
+        protected override void StartInternal()
+        {
+            // Asserts can't be called from here
+            //Assert.AreEqual(ThreadState.Running, WorkingThread.ThreadState);
+        }
+    }
+    class MockThreadedSuspendProcess : ThreadedProcess
+    {
+        private readonly int timeoutMs;
+        public Stopwatch StopWatch { get; set; }
+
+
+        public bool ResumedBeforeTimeout { get; set; }
+
+        internal MockThreadedSuspendProcess(int timeoutMs)
+        {
+            this.timeoutMs = timeoutMs;
+        }
+
+        protected override void StartInternal()
+        {
+            StopWatch = new Stopwatch();
+            StopWatch.Start();
+            ResumedBeforeTimeout = SelfSuspend(TimeSpan.FromMilliseconds(timeoutMs));
+            StopWatch.Stop();
+        }
+    }
+    class MockThreadedBeginStopWithSleepingThreadProcess : ThreadedProcess
+    {
+        public bool ThreadWasInterrupted { get; set; }
+
+        protected override void StartInternal()
+        {
+            // Without the bellow try/catch the test host will go down.
+            //RuntimeHelpers.PrepareConstrainedRegions();
+            try
+            {
+                Thread.Sleep(-1);
+            }
+            catch (ThreadInterruptedException ex)
+            {
+                ThreadWasInterrupted = true;
+                Console.WriteLine("[" + Name + "] Thread interrupted" + ex);
+            }
+        }
+    }
+    class MockAsyncState { public string TestField { get; set; } }
+
+}
