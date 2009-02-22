58ac6ed18a7181122d8c6b722df2d9d29a4d8185
staninprague <staninprague@4c390980-e74c-0410-85aa-cbd269a6c1b3>
2009-02-22 22:42:04 +0000

    Adding the spike project to test multiprocessor msbuild.

---
+++ trunk/Tools.Net/spikes/Versioning/MultiProcBuild/SleepTask.cs
@@ -0,0 +1,40 @@
+﻿
+using Microsoft.Build.Utilities;
+using Microsoft.Build.Framework;
+using System;
+using System.Threading;
+using System.Globalization;
+
+namespace MultiProcBuild
+{
+    /// <summary>
+    /// MSBuild task to delay the process for the required amount of time.
+    /// </summary>
+    /// <remarks>Realized via call to Thread.Sleep.</remarks>
+    public class SleepTask : Microsoft.Build.Utilities.Task
+    {
+        /// <summary>
+        /// Amount of time in milliseconds to sleep.
+        /// </summary>
+        [Microsoft.Build.Framework.Required()]
+        public int Time { get; set; }
+
+        public override bool Execute()
+        {
+            try
+            {
+                Thread.Sleep(Time);
+                
+                Log.LogMessage(MessageImportance.High, String.Format(CultureInfo.InvariantCulture,
+                    "Task {0}, slept for {1} ms.", GetType().Name, Time.ToString()));
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Log.LogErrorFromException(ex, true);
+                return false;
+            }
+        }
+
+    }
+}
