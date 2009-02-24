e3fe7772b125805101970300474db16f129e7a0f
staninprague <staninprague@4c390980-e74c-0410-85aa-cbd269a6c1b3>
2009-02-24 21:44:26 +0000

    ILMerge task is in consistent state. Although conceptually reached yet another cul de sac. This time it is the fact, that when changing assembly version, we want to recompile the dependent assemblies anyway.

---
+++ Tools.Net/spikes/Versioning/IncBuildApp/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using IncBuildLibrary;
+
+namespace IncBuildApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Foo foo = new Foo();
+
+            Console.WriteLine("Message from the IncBuildApp.\r\nPress any key to exit");
+            Console.ReadKey();
+        }
+    }
+}
