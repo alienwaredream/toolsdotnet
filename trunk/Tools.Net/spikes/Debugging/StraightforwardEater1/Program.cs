2bbe98a6df4392cc2d89d56a557a8e426035860e
staninprague <staninprague@4c390980-e74c-0410-85aa-cbd269a6c1b3>
2009-03-09 21:38:03 +0000

    fighting with svn again

---
+++ trunk/Tools.Net/spikes/Debugging/StraightforwardEater1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Globalization;
+
+namespace StraightforwardEater1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int iterations = 10000;
+            int stringSize = 10000;
+
+            var array = new List<string>();
+
+            Console.WriteLine(String.Format(CultureInfo.InvariantCulture, "Going to allocate {0} strings with length {1}",
+                iterations, stringSize));
+
+            for (int i = 0; i < iterations; i++)
+            {
+                array.Add(new String('c', stringSize));
+            }
+
+            Console.WriteLine("Done. Press any key to exit");
+            Console.ReadKey();
+        }
+    }
+}
