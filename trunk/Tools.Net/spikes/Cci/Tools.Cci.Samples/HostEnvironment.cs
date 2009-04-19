41bd0af80bd3ce96a84356352b416f15f580bfc8
staninprague <staninprague@4c390980-e74c-0410-85aa-cbd269a6c1b3>
2009-04-19 20:07:38 +0000

    Adding Cci samples project

---
+++ trunk/Tools.Net/spikes/Cci/Tools.Cci.Samples/HostEnvironment.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.Cci;
+
+namespace Tools.Net.Cci.Samples
+{
+    internal class HostEnvironment : MetadataReaderHost
+    {
+        PeReader peReader;
+        internal HostEnvironment()
+        {
+            this.peReader = new PeReader(this);
+        }
+        public override IUnit LoadUnitFrom(string location)
+        {
+            return this.peReader.OpenModule(BinaryDocument.GetBinaryDocumentForFile(location, this));
+
+        }
+    }
+}
---
+++ trunk/Tools.Net/spikes/Cci/Tools.Cci.Samples/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.IO;
+
+using Microsoft.Cci;
+
+namespace Tools.Net.Cci.Samples
+{
+    class Program
+    {
+        static HostEnvironment env;
+
+        static void Main(string[] args)
+        {
+            if (args == null || args.Length != 2)
+            {
+                Console.WriteLine("Usage: assemblyversion path searchPattern");
+                Console.WriteLine("Usage: assemblyversion \"c:\\product\" *.dll");
+            }
+            else
+            {
+                env = new HostEnvironment();
+
+                AnalyzeDir(args[0], args[1]);
+            }
+        }
+
+        private static void AnalyzeDir(string path, string searchPattern)
+        {
+            if (Directory.Exists(path))
+            {
+                string[] files = Directory.GetFiles(path, searchPattern, SearchOption.AllDirectories);
+
+                foreach (string file in files)
+                {
+                    try
+                    {
+                        IAssembly assembly =
+                            (IAssembly)env.LoadUnitFrom(file);
+
+                        Console.WriteLine(
+                                String.Format("{0}|{1}|{2}",
+                                    assembly.AssemblyIdentity.Location,
+                                    assembly.AssemblyIdentity.Name,
+                                    assembly.AssemblyIdentity.Version));
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(
+                            String.Format("{0}|{1}|{2}",
+                                    file,
+                                    Path.GetFileName(file),
+                                    "N/A"));
+                    }
+                }
+            }
+        }
+    }
+}
