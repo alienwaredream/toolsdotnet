03c649cd9dcd7b5ff22c56632bda51f494bf903b
staninprague <staninprague@4c390980-e74c-0410-85aa-cbd269a6c1b3>
2009-06-03 21:27:55 +0000

    adding ability to map from the sid to user name

--- Tools.Net/src/Tools.SwissKnife/GetUserSid.cs
+++ Tools.Net/src/Tools.SwissKnife/GetUserSid.cs
@@ -11,21 +11,49 @@ namespace Tools.SwissKnife
     {
         internal static void Main(string[] args)
         {
-            if (args.Length < 1)
+            if (args == null || args.Length != 2)
             {
                 System.Console.WriteLine(
-                    "Usage: \"domain\\accountName\"");
+                    "Get sid for the username. Usage: sid \"domain\\accountName\"");
                 System.Console.WriteLine(
-                    "Sample: \"localPC\\localAccount\"");
+                    "Get username for the sid. Usage: un sid");
                 System.Console.WriteLine(
-                    "Sample: \"domain\\domainAccount\"");
+                    "Sample: sid \"localPC\\localAccount\"");
+                System.Console.WriteLine(
+                    "Sample: sid \"domain\\domainAccount\"");
+                System.Console.WriteLine(
+                    "Sample: un S-1-5-21-589166251-1203392894-1708575535-1118");
+
                 return;
 
             }
 
-            NTAccount account = new NTAccount(args[0]);
 
-            System.Console.WriteLine("SID: " + account.Translate(typeof(System.Security.Principal.SecurityIdentifier)).Value);
+            try
+            {
+                if (args.Length == 2 && args[0] == "un")
+                {
+                    SecurityIdentifier sid = new SecurityIdentifier(args[1]);
+
+                    System.Console.WriteLine("User: " + sid.Translate(typeof(System.Security.Principal.NTAccount)).Value);
+                    return;
+
+                }
+
+
+                if (args.Length == 2 && args[0] == "sid")
+                {
+                    NTAccount account = new NTAccount(args[0]);
+
+                    System.Console.WriteLine("SID: " + account.Translate(typeof(System.Security.Principal.SecurityIdentifier)).Value);
+                }
+            }
+            catch (Exception ex)
+            {
+                System.Console.WriteLine("Exception happened when trying to map account for " +
+                    args[1] + ". Exception text" + ex.ToString());
+            }
+
         }
     }
 }
