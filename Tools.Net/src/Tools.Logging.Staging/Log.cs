4393fe7d1e2b0290dcd2c4fd3635c9cf6a750eb4
staninprague <staninprague@4c390980-e74c-0410-85aa-cbd269a6c1b3>
2009-06-06 22:36:04 +0000

    Getting rid of System.Core dependency. That is to support environments without .net 3.5

--- Tools.Net/src/Tools.Logging.Staging/Log.cs
+++ Tools.Net/src/Tools.Logging.Staging/Log.cs
@@ -13,10 +13,19 @@ namespace Tools.Logging
             get { return traceSource; }
         }
 
-        internal static void TraceData(this TraceSource source, TraceEventType eventType,
+        internal static void TraceData(TraceSource source, TraceEventType eventType,
                                        Enum eventId, object data)
         {
             source.TraceData(eventType, Convert.ToInt32(eventId), data);
+        }
+        internal static void TraceData(TraceSource traceSource, TraceEventType traceEventType, int p, string data)
+        {
+            traceSource.TraceData(traceEventType, p, data);
+        }
+
+        internal static void TraceData(TraceSource traceSource, TraceEventType traceEventType, int p, Exception ex)
+        {
+            traceSource.TraceData(traceEventType, p, ex.ToString());
         }
     }
 }
