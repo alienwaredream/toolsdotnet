4be0c73db064b161d30ecc72f1bc48ec2c676a83
staninprague <staninprague@4c390980-e74c-0410-85aa-cbd269a6c1b3>
2009-06-07 22:42:52 +0000

    compilable, but not runnable yet

---
+++ trunk/Tools.Net/spikes/Monitoring/Tools.Coordination.Ems/EmsQueueConfiguration.cs
@@ -0,0 +1,14 @@
+﻿
+namespace Tools.Coordination.Ems
+{
+    public class EMSQueueConfiguration
+    {
+        public string Name { get; set; }
+
+        public QueueType Type { get; set; }
+
+        public string MessageSelector { get; set; }
+
+        public bool NoLocal { get; set; }
+    }
+}
---
+++ trunk/Tools.Net/spikes/Monitoring/Tools.Coordination.Ems/Log.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Tools.Coordination.Ems
+{
+    internal static class Log
+    {
+        private static readonly TraceSource traceSource =
+            new TraceSource((typeof (Log).Assembly.GetName().Name));
+
+        internal static TraceSource Source
+        {
+            get { return traceSource; }
+        }
+
+        internal static void TraceData(TraceSource source, TraceEventType eventType,
+                                       Enum eventId, object data)
+        {
+            try
+            {
+                source.TraceData(eventType, Convert.ToInt32(eventId), data);
+            }
+            catch (Exception ex)
+            {
+                Console.Write(ex.ToString()); // will get into standard output then
+                // this is the lowest fallback possible (SD)
+            }
+            
+        }
+        internal static void TraceData(TraceSource traceSource, TraceEventType traceEventType, int p, string data)
+        {
+            traceSource.TraceData(traceEventType, p, data);
+        }
+    }
+}
