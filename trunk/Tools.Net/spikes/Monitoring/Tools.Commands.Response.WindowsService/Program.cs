4ff040ca5b77d43ed3a334edd6b81a7035e07603
staninprague <staninprague@4c390980-e74c-0410-85aa-cbd269a6c1b3>
2009-06-10 08:56:41 +0000

    commands implementation

---
+++ trunk/Tools.Net/spikes/Monitoring/Tools.Commands.Response.WindowsService/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Diagnostics;
+using Spring.Context.Support;
+using Tools.Processes.Core;
+
+namespace Tools.Commands.Response.WindowsService
+{
+    public class Program : ThreadedProcess
+    {
+        private IProcess process;
+        /// <summary>
+        /// Starts this instance.
+        /// </summary>
+        protected override void StartInternal()
+        {
+            try
+            {
+
+                Log.Source.TraceEvent(TraceEventType.Start, 0, GetType() + " start method called.");
+                process = ContextRegistry.GetContext().GetObject("Coordinator") as IProcess;
+                //Debugger.Launch();
+                process.Start();
+            }
+            catch (Exception ex)
+            {
+                 
+                Log.Source.TraceEvent(TraceEventType.Error, 0, ex.ToString());
+                throw ex;
+            }
+        }
+        /// <summary>
+        /// Stops this instance.
+        /// </summary>
+        public override void Stop()
+        {
+            process.Stop();
+
+            base.Stop(); // base stop should 
+            Log.Source.TraceEvent(TraceEventType.Start, 0, GetType() + " Stop method called.");
+        }
+    }
+}
---
+++ trunk/Tools.Net/spikes/Monitoring/Tools.Commands.Response.WindowsService/UninstallGenericService.cmd
@@ -0,0 +1,2 @@
+"%windir%\Microsoft.NET\Framework\v2.0.50727\InstallUtil" /u C:\FORIS\Commands\Tools.Commands.Response.WindowsService.exe
+@pause
