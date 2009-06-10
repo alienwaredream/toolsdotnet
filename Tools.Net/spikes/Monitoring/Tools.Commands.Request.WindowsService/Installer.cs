a176e04d0b5c45b1b881085dabb022f665302b68
staninprague <staninprague@4c390980-e74c-0410-85aa-cbd269a6c1b3>
2009-06-10 08:55:39 +0000

    commands implementation

---
+++ Tools.Net/spikes/Monitoring/Tools.Commands.Request.WindowsService/Installer.cs
@@ -0,0 +1,39 @@
+using System.ComponentModel;
+using System.Diagnostics;
+
+namespace Tools.Commands.Request.WindowsService
+{
+    [RunInstaller(true)]
+    public partial class Installer : Tools.Processes.Host.Installer
+    {
+        public Installer()
+        {
+            InitializeComponent();
+
+            SetupCounters();
+
+            SetupEventLog();
+        }
+
+        protected void SetupEventLog()
+        {
+            // Create an instance of an EventLogInstaller.
+            var eventLogInstaller = new EventLogInstaller();
+
+            // Set the source name of the event log.
+            eventLogInstaller.Source = "Mts-Commands";
+
+            // Set the event log that the source writes entries to.
+            eventLogInstaller.Log = "Mts-Commands";
+
+            // Add eventLogInstaller to the Installer collection.
+            Installers.Add(eventLogInstaller);
+
+        }
+
+        protected void SetupCounters()
+        {
+
+        }
+    }
+}
