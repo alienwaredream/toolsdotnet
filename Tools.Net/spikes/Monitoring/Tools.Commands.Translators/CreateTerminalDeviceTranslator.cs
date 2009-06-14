8f8a54f86454bcc2a2e2274fefc637dae3b11dc3
staninprague <staninprague@4c390980-e74c-0410-85aa-cbd269a6c1b3>
2009-06-14 19:22:25 +0000

    getting closer to have more robust CommandExecutor

--- Tools.Net/spikes/Monitoring/Tools.Commands.Translators/CreateTerminalDeviceTranslator.cs
+++ Tools.Net/spikes/Monitoring/Tools.Commands.Translators/CreateTerminalDeviceTranslator.cs
@@ -3,11 +3,17 @@ using Tools.Commands.Implementation;
 using Tools.Commands.Implementation.IF1.Ctd;
 using Tools.Core.Utils;
 using System.Collections.Generic;
+using Tools.Core.Asserts;
+using System.Globalization;
+using System.Xml.Schema;
+using System.Xml;
+using System.IO;
 
 namespace Tools.Commands.Translators
 {
     public class CreateTerminalDeviceTranslator : ICommand2MessageTranslator
     {
+        //public string XsdPath { get; set; }
 
         #region ICommand2MessageTranslator Members
 
@@ -17,21 +23,51 @@ namespace Tools.Commands.Translators
 
             ctd.req = new req();
 
-            ctd.req.reqId = command.ReqId.ToString();
+            ctd.req.reqId = command.ReqId.ToString(CultureInfo.InvariantCulture);
+            ctd.req.TIScustomerId = command.TisCustomerId;
+            ctd.req.TISTDid = command.TisTDId.ToString(CultureInfo.InvariantCulture);
+            ctd.req.TISwalletId = command.TisWalletId;
 
-            CreateTerminalDevice createTD = new CreateTerminalDevice();
+            List<addOnMPs> addOnMPs = new List<addOnMPs>();
+            bool isBaseMPSet = false;
 
-
-            bool baseMPSet = false;
             //TODO: (SD) can't use extension methods
             foreach (MarketingPackage mp in command.MarketingPackages)
             {
                 // Base package is discriminated by the BASE MPType
+                // Only allow one base marketing package, more than one is an error.
                 if (mp.MPType.ToUpper() == "BASE")
                 {
-                    baseMPSet = true;
+                    if (ErrorTrap.AddAssertion(!isBaseMPSet, CommandValidationError.DuplicateBaseMarketingPackage))
+                    {
 
-                    baseMP bmp = new baseMP { id = mp.MPId.ToString() };
+                        isBaseMPSet = true;
+
+                        baseMP bmp = new baseMP { id = mp.MPId.ToString() };
+
+                        List<@params> parameters = new List<@params>();
+
+                        foreach (PackageParameter pp in mp.Parameters)
+                        {
+                            parameters.Add(new @params
+                            {
+                                code = pp.ParamCode,
+                                productCode = pp.ProductCode,
+                                value = pp.Value
+                            });
+                        }
+
+                        bmp.@params = parameters.ToArray();
+
+                        ctd.req.baseMP = bmp;
+                    }
+
+                    continue;
+                }
+                // Base package is discriminated by the BASE MPType
+                if (mp.MPType.ToUpper() == "ADDON")
+                {
+                    addOnMPs aomp = new addOnMPs { id = mp.MPId.ToString() };
 
                     List<@params> parameters = new List<@params>();
 
@@ -43,89 +79,97 @@ namespace Tools.Commands.Translators
                             productCode = pp.ProductCode,
                             value = pp.Value
                         });
-
-
-
                     }
 
-                    bmp.@params = parameters.ToArray();
+                    aomp.@params = parameters.ToArray();
 
-                    ctd.req.baseMP = bmp;
+                    addOnMPs.Add(aomp);
+
+                    continue;
+                }
+
+                ctd.req.addOnMPs = addOnMPs.ToArray();
+            }
+
+            ctd.req.contractEnd = command.ContractEndDate;
+            ctd.req.monthlyLimit = Convert.ToDouble(command.MonthlyLimit);
+            ctd.req.reqTIme = command.ReqTime;
+            ctd.req.p2p = command.P2P;
+
+            bool canSerialize = true;
+
+            if (ErrorTrap.AddAssertion(Enum.IsDefined(typeof(TDtype), command.TDType.ToString()),
+                String.Format("Command TDType value {0} doesn't fall in the allowed range.", command.TDType)))
+            {
+
+                ctd.req.TDtype = (TDtype)command.TDType;
+            }
+            else
+            {
+                canSerialize = false;
+            }
+
+            TDelements tdElements = new TDelements();
+
+            //TODO: (SD) Find out if this is required actually or not
+            if (!String.IsNullOrEmpty(command.IccId))
+            {
+                mobile m = new mobile();
+                m.ICCID = command.IccId;
+                m.phoneNumber = command.PhoneNumber;
+                m.profile = command.VpnProfile.ToString();
+                m.shortNumber = command.ShortNumber;
+
+                tdElements.Item = m;
+                ctd.req.TDelements = tdElements;
+            }
+
+            //ErrorTrap.AddAssertion(false, "test assert failed!");
+            // If there are any errors accumulated, raise them now.
+            //ErrorTrap.RaiseTrappedErrors<CommandValidationError>();
+            string messageText = null;
+
+            if (canSerialize)
+            {
+
+                messageText = SerializationUtility.Serialize2String(ctd.req);
+
+                // if xsd is provided, execute xsd validation
+                //if (!String.IsNullOrEmpty(XsdPath))
+                //{
+                XmlSchemaSet sc = new XmlSchemaSet();
+
+                //// Add the schema to the collection.
+                sc.Add("http://www.tibco.com/schemas/SDPRO_Observer/Observer/SharedResources/XSD/IF1/AllTypes.xsd", @"IF1\xsd\AllTypes.xsd");
+                sc.Add("http://www.tibco.com/schemas/SDPRO_Observer/Observer/SharedResources/XSD/IF1/CreateTerminalDevice.xsd", @"IF1\xsd\CreateTerminalDevice.xsd");
+
+                // Set the validation settings.
+                XmlReaderSettings settings = new XmlReaderSettings();
+                settings.ValidationType = ValidationType.Schema;
+                settings.Schemas = sc;
+                settings.ValidationEventHandler += new ValidationEventHandler(ValidationCallBack);
+
+                using (StringReader sReader = new StringReader(messageText))
+                {
+                    using (XmlReader reader = XmlReader.Create(sReader, settings))
+                    {
+                        while (reader.Read()) ;
+                    }
                 }
             }
+            //}
 
             return new MessageShim
             {
                 CorrelationId = command.ReqId.ToString(),
-                Text = SerializationUtility.Serialize2String(ctd.req)
+                Text = messageText
             };
+        }
 
-            //for (Iterator<MPInstance> iter = request.getMarketingPackages().iterator(); iter.hasNext();) {
-            //    mpInstance = iter.next();
-            //    if (mpInstance instanceof BaseMPInstance) {
-            //        baseMP.setId(new BigInteger(String.valueOf(mpInstance.getMarketingPackageId())));
-            //        for (Param param : mpInstance.getParams()) {
-            //            com.telekomsrbija.foris.commandtypes.alltypes.Params p = 
-            //                new com.telekomsrbija.foris.commandtypes.alltypes.Params();
-            //            p.setCode(param.getPk().getParamCode());
-            //            p.setProductCode(param.getPk().getProductCode());
-            //            p.setValue(param.getValue());
-
-            //            baseMP.getParams().add(p);
-            //        }
-            //    }
-
-            //    /*if (mpInstance instanceof AddonInstance) {
-            //        AddOnMPs addon = new AddOnMPs();
-            //        addon.setId(new BigInteger(String.valueOf(mpInstance.getMarketingPackageId())));
-            //        for (Param param : mpInstance.getParams()) {
-            //            com.telekomsrbija.foris.commandtypes.alltypes.Params p = 
-            //                new com.telekomsrbija.foris.commandtypes.alltypes.Params();
-            //            p.setCode(param.getPk().getParamCode());
-            //            p.setProductCode(param.getPk().getProductCode());
-            //            p.setValue(param.getValue());
-
-            //            addon.getParams().add(p);
-            //        }
-            //        req.getAddOnMPs().add(addon);
-            //    }*/
-            //}
-
-
-            //// treba dodati preslikavanje za params
-            //// treba dodati i req.getAddonMPs - ovo proveriti kako ce ici
-            //req.setBaseMP(baseMP);
-            //req.setContractEnd(
-            //        BasicConverter.timestampToXMLGregorianCalendar(request.getContractEndDate()));
-            //req.setMonthlyLimit(request.getMonthlyLimit());
-            //req.setReqId(request.getRequestId().toString());
-            //req.setReqTIme(
-            //        BasicConverter.timestampToXMLGregorianCalendar(request.getRequestTime()));
-            //req.setTDtype(String.valueOf(request.getTdType().toInt()));
-            //req.setP2p(request.getP2P());
-
-            //TDelements tdElements = new TDelements();
-            //if (request.getTdElements().getIccid() != null) {
-            //    Mobile mobile = new Mobile();
-            //    mobile.setICCID(request.getTdElements().getIccid());
-            //    mobile.setPhoneNumber(request.getTdElements().getPhoneNumber());
-            //    mobile.setProfile(request.getTdElements().getProfile());
-            //    mobile.setShortNumber(request.getTdElements().getShortNumber());
-
-            //    tdElements.setMobile(mobile);
-            //}
-
-            //req.setTDelements(tdElements);
-
-            //req.setTISCustomerId(request.getTisCustomerId());
-            //req.setTISTDid(request.getTisTerminalDeviceId());
-            //req.setTISwalletId(request.getTisWalletId());
-
-            //createTD.setReq(req);
-
-            //out.add("payload", req);
-            //out.add("id", req.getReqId());
-            //out.add("commandName", createTD.getClass().getSimpleName());
+        // Display any validation errors.
+        private static void ValidationCallBack(object sender, ValidationEventArgs e)
+        {
+            ErrorTrap.AppendText("xsd error at pos(" + e.Exception.LineNumber + "," + e.Exception.LinePosition + ") " + e.Message + ". Schema: " + e.Exception.SourceSchemaObject);
         }
 
         #endregion
