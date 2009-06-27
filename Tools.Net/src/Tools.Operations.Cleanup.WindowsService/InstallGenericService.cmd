f67713577b409f56e21233655dd14b3729970c80
staninprague <staninprague@4c390980-e74c-0410-85aa-cbd269a6c1b3>
2009-06-27 14:48:41 +0000

    Staring project to introduce basic cleanup functionality - archive files - zip and move them.

---
+++ Tools.Net/src/Tools.Operations.Cleanup.WindowsService/InstallGenericService.cmd
@@ -0,0 +1,2 @@
+"%windir%\Microsoft.NET\Framework\v2.0.50727\InstallUtil" C:\FORIS\CleanupService\Tools.Operations.Cleanup.WindowsService.exe
+@pause
---
+++ Tools.Net/src/Tools.Operations.Cleanup.WindowsService/Log.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+using System.Text;
+using System.Diagnostics;
+
+namespace Tools.Operations.Cleanup.WindowsService
+{
+    internal static class Log
+    {
+        private static TraceSource traceSource =
+            new TraceSource((typeof(Log).Assembly.GetName().Name));
+
+        internal static TraceSource Source { get { return traceSource; } }
+
+        internal static void TraceData(TraceSource source, TraceEventType eventType,
+            Enum eventId, object data)
+        {
+            source.TraceData(eventType, Convert.ToInt32(eventId), data);
+        }
+
+    }
+}
