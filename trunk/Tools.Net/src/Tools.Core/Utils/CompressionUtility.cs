21d01a2f3c6612e3eacdc6f42a7daff1d584313d
staninprague <staninprague@4c390980-e74c-0410-85aa-cbd269a6c1b3>
2009-06-29 13:18:42 +0000

    Cleanup and zip projects

---
+++ trunk/Tools.Net/src/Tools.Core/Utils/CompressionUtility.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Text;
+using System.IO;
+using System.IO.Compression;
+using Tools.Core.Asserts;
+
+namespace Tools.Core.Utils
+{
+    public static class CompressionUtility
+    {
+        public static void CompressFile(string sourceFilePath, string targetFilePath)
+        {
+            ErrorTrap.AddAssertion(File.Exists(sourceFilePath), String.Format("Source file [{0}] doesn't exist!", sourceFilePath));
+            ErrorTrap.AddAssertion(!File.Exists(targetFilePath), String.Format("Target file [{0}] already exists!", sourceFilePath));
+
+            ErrorTrap.RaiseTrappedErrors<InvalidOperationException>();
+
+            using (FileStream targetFileStream = new FileStream(targetFilePath, FileMode.CreateNew, FileAccess.Write))
+            {
+                using (Stream zipStream = new GZipStream(targetFileStream, CompressionMode.Compress, true))
+                {
+                    using (FileStream sourceFileStream = new FileStream(sourceFilePath, FileMode.Open, FileAccess.Read))
+                    {
+
+                        byte[] buffer = new byte[sourceFileStream.Length];
+
+                        sourceFileStream.Read(buffer, 0, buffer.Length);
+
+                        zipStream.Write(buffer, 0, buffer.Length);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ trunk/Tools.Net/src/Tools.Zip/ZipConstants.cs
@@ -0,0 +1,54 @@
+using System;
+
+namespace Tools.Zip
+{
+	/// <summary>
+	/// This class contains ZipConstants used for gzip.
+	/// </summary>
+	sealed class ZipConstants
+	{
+		private ZipConstants(){
+		}
+		public const byte DEFLATE = 4;	//Indicating deflate method of compression
+		public const byte GZIP = 6;		//Indicating gzip method of compression
+
+		public const int FixedHeaderSize = 30; //Fixed part of the header
+		public const int SuperHeaderSize = 3;
+
+		public const string AddFiles = "Add files";
+		public const string NewArchive = "New archive";
+		public const string OpenArchive = "Open archive";
+		//Extensions for the files
+		public const string Extension = "xip files (*.xip)|*.xip";
+		public const string AllExtensions = "All files (*.*)|*.*";
+
+		public const string BackSlash = "\\";
+		public const string Dot = ".";
+
+		public const string ExtractMessage = 
+								"Extracted files to {0} successfully.";
+		public const string OpenMessage = "Opened archive {0} for editing" ;
+		public const string NewMessage = "Opened new archive {0} for editing";
+		public const string AddMessage = "Added files to the archive.";
+
+		public const string Title = "Compression sample";
+		public const string GzipName = "GZip";
+		public const string DeflateName = "Deflate";
+		public const string ErrorName = "Error";
+
+		public const string SeekError = "Attempted to seek before the beginning";
+		public const string IOError = "An IO error has occured";
+		public const string CloseError = "Stream already closed";
+		public const string MemoryError = "Running out of memory";
+		public const string ArgumentError = "Argument invalid";
+		public const string FileNotFoundError = "File not found";
+		public const string FileError = "Filename invalid";
+		public const string CorruptedError = 
+				"Trying to read a corrupted xip file";
+		public const string FileExistsError = "File already exists";
+
+		public const string FileReplace = 
+			"The file chosen already exists. Do you want to replace it?";
+		public const string Replace = "Replace";
+	}
+}
