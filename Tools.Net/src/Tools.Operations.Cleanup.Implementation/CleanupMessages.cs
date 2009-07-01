ccf98368820ee34a5f3920efa8b029e631b00f51
staninprague <staninprague@4c390980-e74c-0410-85aa-cbd269a6c1b3>
2009-07-01 21:18:21 +0000

    completing the archiving/zipping part of the cleanup service.

--- Tools.Net/src/Tools.Operations.Cleanup.Implementation/CleanupMessages.cs
+++ Tools.Net/src/Tools.Operations.Cleanup.Implementation/CleanupMessages.cs
@@ -5,6 +5,11 @@ namespace Tools.Operations.Cleanup.Implementation
     internal enum CleanupMessages
     {
         CleanupIterationStarted = 18001,
-        ArchiveDirectoryDoesntExist = 18002
+        ArchiveDirectoryDoesntExist = 18002,
+        FilesArchived = 18003,
+        ArchiveWithSameNameAlreadyExisted = 18004,
+        NothingToArchive = 18005,
+
+        ErrorWhileArchivingTheFile = 18051,
     }
 }
