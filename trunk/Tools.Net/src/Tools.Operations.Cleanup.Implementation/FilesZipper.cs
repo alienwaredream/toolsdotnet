35542158e8ad283bf12f0617a6413e7ec9430640
staninprague <staninprague@4c390980-e74c-0410-85aa-cbd269a6c1b3>
2009-07-01 21:18:21 +0000

    completing the archiving/zipping part of the cleanup service.

--- trunk/Tools.Net/src/Tools.Operations.Cleanup.Implementation/FilesZipper.cs
+++ trunk/Tools.Net/src/Tools.Operations.Cleanup.Implementation/FilesZipper.cs
@@ -9,16 +9,27 @@ namespace Tools.Operations.Cleanup.Implementation
 
     using SharpZip = ICSharpCode.SharpZipLib;
     using System.Text;
+    using System.Diagnostics;
 
     internal class FilesZipper : ScheduleTaskProcessor
     {
         string directoryToArchive;
         int daysToArchive = 2;
         string zipDirectory;
+        string timestampFormat = "dd-MMM-yy_HH-mm-ss";
 
         protected override void ExecuteSheduleTask()
         {
-            Log.TraceData(Log.Source, System.Diagnostics.TraceEventType.Information, CleanupMessages.CleanupIterationStarted, "Started archival iteration");
+            Guid archivationGuid = Guid.NewGuid();
+            StringBuilder sb = new StringBuilder();
+            string zipFilePath = null;
+            DateTime dateThreshold = DateTime.Now.AddDays(-daysToArchive);
+
+            Trace.CorrelationManager.ActivityId = archivationGuid;
+            Log.TraceData(Log.Source, TraceEventType.Start, CleanupMessages.CleanupIterationStarted,
+                String.Format(
+                "Started archival iteration for directory [{0}] with modified date threshold [{1}]. Archivation target directory is [{2}]",
+                directoryToArchive, dateThreshold.ToString(timestampFormat), zipDirectory));
 
             if (String.IsNullOrEmpty(directoryToArchive))
             {
@@ -33,32 +44,69 @@ namespace Tools.Operations.Cleanup.Implementation
             }
 
             string[] files = Directory.GetFiles(directoryToArchive);
-            StringBuilder sb = new StringBuilder("Archivation results: \r\n");
 
             foreach (string file in files)
             {
                 try
                 {
+
                     FileInfo fi = new FileInfo(file);
 
-                    if (fi.LastWriteTime < DateTime.Now.AddDays(-daysToArchive))
+
+                    if (fi.LastWriteTime < dateThreshold)
                     {
-                        using (SharpZip.Zip.ZipFile target = SharpZip.Zip.ZipFile.Create(zipDirectory + @"\" + file + ".zip"))
+
+                        zipFilePath = zipDirectory.TrimEnd('\\') + @"\" + fi.Name +
+                            "-" + fi.LastWriteTime.ToString(timestampFormat) + ".zip";
+
+                        if (!File.Exists(zipFilePath))
                         {
 
-                            target.BeginUpdate();
+                            using (SharpZip.Zip.ZipFile target = SharpZip.Zip.ZipFile.Create(zipFilePath))
+                            {
 
-                            target.Add(file);
+                                target.BeginUpdate();
 
-                            target.CommitUpdate();
+                                target.Add(file, fi.Name +
+                                    "-" + fi.LastWriteTime.ToString(timestampFormat) + fi.Extension);
+
+                                target.CommitUpdate();
+
+                                if (sb.Length == 0)
+                                    sb.Append("Archivation results: \r\n");
+                                sb.Append(String.Format("File [{0}] archived to [{1}]", file, zipFilePath));
+                                sb.Append(Environment.NewLine);
+                            }
+
+                            File.Delete(file);
+                        }
+                        else
+                        {
+                            Log.TraceData(Log.Source, System.Diagnostics.TraceEventType.Warning, CleanupMessages.ArchiveWithSameNameAlreadyExisted,
+                                String.Format("Archive [{0}] for file [{1} (last modified date:{2})] already exists!",
+                                zipFilePath, file, fi.LastWriteTime.ToString("dd-MMM-yy_HH-mm-ss")));
                         }
 
                     }
                 }
                 catch (Exception ex)
                 {
+                    Log.TraceData(Log.Source, System.Diagnostics.TraceEventType.Error, CleanupMessages.ErrorWhileArchivingTheFile,
+                        String.Format("Error while archiving the file [{0}] to zip file [{1}]. \r\nException text: {2}", file, zipFilePath, ex.ToString()));
 
                 }
+
+            }
+            if (sb.Length > 0)
+            {
+                Log.TraceData(Log.Source, System.Diagnostics.TraceEventType.Information, CleanupMessages.FilesArchived,
+                    sb.ToString());
+            }
+            else
+            {
+                Log.TraceData(Log.Source, System.Diagnostics.TraceEventType.Information, CleanupMessages.NothingToArchive,
+                    String.Format("There was nothing to archive in [{0}]. Modified date threshold was [{1}]",
+                    directoryToArchive, dateThreshold.ToString(timestampFormat)));
             }
         }
     }
